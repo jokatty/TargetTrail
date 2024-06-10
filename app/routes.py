8e2d1a8386d7f16c235505e5e2d1252a6b4c2475
jokatty <jyotikattani@gmail.com>
2024-06-10 13:58:19 +0200

    add new goal route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, render_template
 from sqlalchemy.orm import sessionmaker
 from .models import Goal, init_db
 
@@ -11,3 +11,19 @@ session = Session()
 @bp.route('/')
 def index():
     return "Hello, World!"
+
+@bp.route('/new_goal', methods=['GET', 'POST'])
+def new_goal():
+    if request.method == 'POST':
+        goal = request.form['goal']
+        duration = request.form['duration']
+        duration_unit = request.form['duration_unit']
+        if duration_unit == 'hours':
+            duration = int(duration) * 60
+        duration = int(duration)
+        new_goal = Goal(goal=goal, duration=duration)
+        session.add(new_goal)
+        session.commit()
+        return redirect('/')
+    return render_template('new_goal.html')
+
