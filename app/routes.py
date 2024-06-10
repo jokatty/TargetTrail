0fbbdeed6ebf4005dcc9d3ee6026ba0bc00d6fff
jokatty <jyotikattani@gmail.com>
2024-06-10 13:10:27 +0200

    set up index route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import Blueprint
+from sqlalchemy.orm import sessionmaker
+from .models import Goal, init_db
 
 bp = Blueprint('main', __name__)
+
+engine = init_db()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @bp.route('/')
 def index():
