87029dc5a5b948c1037edefa1fb46c02795b40d7
jokatty <jyotikattani@gmail.com>
2024-06-10 13:11:13 +0200

    set up goal model

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Goal(Base):
+    __tablename__ = 'goals'
+    id = Column(Integer, primary_key=True)
+    goal = Column(String)
+    duration = Column(Integer)
+
+def init_db():
+    engine = create_engine('sqlite:///target.db')
+    Base.metadata.create_all(engine)
+    return engine
